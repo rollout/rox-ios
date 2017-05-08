71607e50cc6baea02be1d778ff69e3afe7bc0b4c
elad cohen <eladcohen@Elads-MacBook-Pro.local>
2017-05-08 14:37:23 +0300

    version 0.5.0, build 380

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-rollout.io-ios
+ROX
 ==============
 
-##Hot patch critical bugs in production apps
-Stop waiting for AppStore approvals just to fix bugs. Search your app's live code and fix, contain or switch off functions whenever you want.
+## Continuous Feature Rollouts for Mobile Release
+
+Release mobile features quickly and safely with fully controlled rollouts, measure impact, and react as needed without waiting for your next code release.
 
 ## How to get started
 Visit http://www.rollout.io/ to add ROX SDK to your app
+
--- ROX/ROX.framework/Versions/A/Headers/ROX.h
+++ ROX/ROX.framework/Versions/A/Headers/ROX.h
@@ -1,5 +1,5 @@
 //
-//  ROX SDK version 0.4.1, Build 376
+//  ROX SDK version 0.5.0, Build 380
 //
 //  Copyright (c) 2014 ROX.io. All rights reserved.
 //
@@ -20,17 +20,20 @@
 + (UIViewController*)featuresViewController;
 + (BOOL)isTestDevice;
 
-+(void) setUserStringProperty:(NSString*)value forKey:(NSString*)key;
-+(void) setUserComputedStringProperty:(NSString* (^)())block forKey:(NSString*)key;
++(void) setCustomStringProperty:(NSString*)value forKey:(NSString*)key;
++(void) setCustomComputedStringProperty:(NSString* (^)())block forKey:(NSString*)key;
 
-+(void) setUserBooleanProperty:(BOOL)value forKey:(NSString*)key;
-+(void) setUserComputedBooleanProperty:(BOOL (^)())block forKey:(NSString*)key;
++(void) setCustomBooleanProperty:(BOOL)value forKey:(NSString*)key;
++(void) setCustomComputedBooleanProperty:(BOOL (^)())block forKey:(NSString*)key;
 
-+(void) setUserIntProperty:(int)value forKey:(NSString*)key;
-+(void) setUserComputedIntProperty:(int (^)())block forKey:(NSString*)key;
++(void) setCustomIntProperty:(int)value forKey:(NSString*)key;
++(void) setCustomComputedIntProperty:(int (^)())block forKey:(NSString*)key;
 
-+(void) setUserDoubleProperty:(double)value forKey:(NSString*)key;
-+(void) setUserComputedDoubleProperty:(double (^)())block forKey:(NSString*)key;
++(void) setCustomDoubleProperty:(double)value forKey:(NSString*)key;
++(void) setCustomComputedDoubleProperty:(double (^)())block forKey:(NSString*)key;
+
++(void) setCustomSemverProperty:(NSString*)value forKey:(NSString*)key;
++(void) setCustomComputedSemverProperty:(NSString* (^)())block forKey:(NSString*)key;
 
 +(BOOL) isInTargetGroup:(NSString*)name;
 +(NSArray<ROXTargetGroup *>*) targetGroups;
