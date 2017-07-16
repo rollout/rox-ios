3b4b4416a581ef6307fbc356ae83d14ec45c4f5f
elad cohen <eladcohen@Elads-MacBook-Pro.local>
2017-07-16 17:46:50 +0300

    version 1.0.4, build 432

--- RoxSdk/RoxContainer.swift
+++ RoxSdk/RoxContainer.swift
@@ -1,14 +1,14 @@
 import Foundation
 
 /**
- A container class for configurations and flags 
+ A container class for configurations and flags
  
- Any swift class that adopts this protocol can be used as an entry point for flags and configurations that are controlled by ROX system. To use an instance of this class you need to register the instance using the `ROX.register(_:)` method and retrieve the instance using `ROX.get(_:)` method
+ Any swift class that adopts this protocol can be used as an entry point for flags and containers that are controlled by ROX system. To use an instance of this class you need to register the instance using the `ROX.register(_:)` method and retrieve the instance using `ROX.get(_:)` method
  
- Here is an example of a class using flags and configuration together:
+ Here is an example of a class using flags and containers together:
  
  ```swift
-     public class MyConfiguration : ROXConfiguration {
+     public class MyContainer : RoxContainer {
         //group support feature flag
         let groupSupport = RoxFlag()
  
@@ -25,12 +25,12 @@ import Foundation
         let ratio = RoxConfigurationDouble(defaultValue: 0.55)
      }
  
-    // Register MyConfiguration class
-    ROX.register(MyConfiguration())
+    // Register MyContainer class
+    ROX.register(MyContainer())
  
  
-    // Access MyConfiguration from any
-    let conf = ROX.get(MyConfiguration.self)!
+    // Access MyContainer from any
+    let conf = ROX.get(MyContainer.self)!
  
     // Use the Flag
     conf.groupSupport.enabled {
@@ -42,14 +42,14 @@ import Foundation
 
 ```
  */
-public protocol RoxConfiguration {
+public protocol RoxContainer : class {
     /**
         Override this property to override the default namespace of a class
     */
     var namespace: String? { get }
 }
 
-public extension RoxConfiguration {
+public extension RoxContainer {
     /**
      The default implementation of namespace is to return the class full name , you can use [nested type](https://developer.apple.com/library/content/documentation/Swift/Conceptual/Swift_Programming_Language/NestedTypes.html#//apple_ref/doc/uid/TP40014097-CH23-ID242) to create a namespace 
      */
